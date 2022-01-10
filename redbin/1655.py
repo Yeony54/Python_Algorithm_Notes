00a4bc8dccf9ef57a555ac8c01a05217c3b5eca4
YEONY54 <tjwldus425o@naver.com>
2022-01-11 01:12:18 +0900

    -

---
+++ redbin/1655.py
@@ -0,0 +1,17 @@
+# 방법 1
+# 들어오는 값 차례로 정렬 (list)
+# list 길이가 2 이하일때 list[0] 출력
+# list 길이가 짝수 일때 list[len(list//2)-1] 를 중간값으로 출력
+# list 길이가 홀수 일때 list[len(list//2)] 를 중간값으로 출력
+
+# list 정렬을 어떻게 할지 모르겠음
+
+N = input()
+list = []
+
+
+list.append(input())
+
+    
+
+    

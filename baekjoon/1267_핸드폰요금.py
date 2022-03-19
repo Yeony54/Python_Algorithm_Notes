9ee87021a77e11a9d8eb72f070209194144e326e
Yeony54 <tjwldus425o@naver.com>
2022-03-20 01:47:05 +0900

    0319

--- baekjoon/1267_핸드폰요금.py
+++ baekjoon/1267_핸드폰요금.py
@@ -1,14 +1,15 @@
 import sys
-import numpy as np
 
-while(1):
-    N = int(sys.stdin.readline())
-    arr = list(map(int, sys.stdin.readline().split()))
+N = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
 
-    calls = np.array(arr)
-    ys = (calls + 31)//30
-    ys = ys.sum() * 10
-    ms = (calls + 61)//60
-    ms = ms.sum() * 15
+y_sum = 0
+m_sum = 0
 
-    print(f'Y {ys}' if ys<ms  else  f'Y M {ms}' if ys==ms  else f'M {ms}' )
+for call in arr:
+    y_sum += (call+30)//30
+    m_sum += (call+60)//60
+ys = y_sum*10
+ms = m_sum*15
+
+print(f'Y {ys}' if ys<ms  else f'Y M {ms}'if ys==ms  else f'M {ms}')
